194693d1baf19ac3caf9a7d35bfb08ba27c8b75d
Beliz Pehlivan <belizpehlivan@gmail.com>
2021-12-18 21:34:47 +0300

    student courses

--- student/courses.php
+++ student/courses.php
@@ -10,9 +10,43 @@
         <div class="main-content">
             <div class="wrapper">
                 <h1>Courses</h1>
-                <div class="course">
-                    <h4></h4>
-                </div>
+                <?php
+                    $act_user = $_SESSION['user'];
+                    $sql = "SELECT * FROM student WHERE username = '$act_user'";
+                    $res = mysqli_query($conn, $sql);
+                    if($res==TRUE){
+                        $count = mysqli_num_rows($res);
+                        if($count == 1){
+                            $rows = mysqli_fetch_assoc($res);
+                            $id = $rows['id'];
+
+                            $sql2 = "SELECT * FROM course_student WHERE student_id = '$id'";
+                            $res2 = mysqli_query($conn, $sql2);
+                            if($res2==TRUE){
+                                $count2 = mysqli_num_rows($res2);
+                                if($count2 > 0){
+                                    while($rowscourse = mysqli_fetch_assoc($res2)){
+                                        $course_code = $rowscourse['course_code'];
+                                        ?>
+                                            <div class="course">
+                                                <a href=""></a>
+                                            <h4><?php echo $course_code; ?></h4>
+                                            </div>
+                                        <?php
+                                    }
+                                }else{
+                                     //no course
+                                     echo "no course";
+                                }
+                            }
+                        }
+                        else{
+                                //
+                        }
+                        
+                    }
+                ?>
+                
                 <div class="clearfix"></div>
             </div>
         </div>
